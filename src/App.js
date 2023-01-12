3dd83835e3c91010b0a7322f2673281ec7d1c23b
devgenx <sebastian110420@gmail.com>
2023-01-12 12:52:07 -0500

    Refactored guide for the lecture

--- src/App.js
+++ src/App.js
@@ -11,15 +11,20 @@ const App = () => {
   const [searchField, setSearchField] = useState("");
   const [filteredIngredients, setFilteredIngredients] = useState(Ingredients);
 
+  // handles the input field from the page
+  // set the value of the input to be the new searchField value using setSearchField
   const handleInputChange = (e) => {
     setSearchField(e.target.value);
   };
 
+  // use Effect will fire when the whole application is rendered
   useEffect(() => {
     const newFilteredIngredients = Ingredients.filter((ingredient) => {
       return ingredient.toLowerCase().includes(searchField.toLowerCase());
     });
+    // set the newFilteredIngredients to be the new filteredIngredients using setFilteredIngredients
     setFilteredIngredients(newFilteredIngredients);
+    // When the app detected changes from Ingredients(when form was submitted or there is an input value, this effect will fire)
   }, [Ingredients, searchField]);
 
   return (
--- src/Components/form.js
+++ src/Components/form.js
@@ -1,11 +1,17 @@
 import React from "react";
 
+// Pizza form
+
 const pizzaForm = ({ Ingredients, setIngredients }) => {
+  // Handles the submit event
   const handleSubmit = (e) => {
+    // new ingredient is the value being passed-in in the input
     const newIngredient = e.target[0].value;
     e.preventDefault();
 
     // return of an array
+    // setIngredients is from the state,
+    // spread the values of the Ingredients array then add the new ingredient
     setIngredients([...Ingredients, newIngredient]);
   };
   return (
